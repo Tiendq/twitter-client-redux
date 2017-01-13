b1a05e4c676d31b0bfebcdc52d5799d374e63459
Tien Do <tiendq@gmail.com>
2017-01-13 15:01:01 +0700

    Update comment

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,5 @@
-// Create a new Twitter application here https://apps.twitter.com/app/new
+// You need create a new Twitter application here https://apps.twitter.com/app/new
+// and replace placeholders with real values.
 
 const CONSUMER_KEY = 'CONSUMER_KEY';
 const CONSUMER_SECRET = 'CONSUMER_SECRET';
