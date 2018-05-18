dc0fddb4a6be54a0077442d46bd324d08e212fd3
Tien Do <tiendq@gmail.com>
2018-05-18 20:14:44 +0700

    fix: add demo keys to test file

--- scripts/twitter-client.test.js
+++ scripts/twitter-client.test.js
@@ -1,12 +1,21 @@
-let config = require('dotenv').config();
 import { getBearerToken } from './twitter-client';
 
-describe('Authenticate with getBearerToken', () => {
-  test('should return a valid bearer token', () => {
-    expect.assertions(2);
-    return getBearerToken(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET).then(response => {
-      expect(response.token_type).toBe('bearer');
-      expect(response.access_token).toBeDefined();
-    });
+const CONSUMER_KEY = '1doYirbYlY0kpF3v0R120p2uH';
+const CONSUMER_SECRET = 'cqUEMyzBiGQd4RE8U8M2hSwwDz73wGVwNwnIDP73wdmOgUDutD';
+
+test('should return a valid bearer token', () => {
+  expect.assertions(2);
+
+  return getBearerToken(CONSUMER_KEY, CONSUMER_SECRET).then(response => {
+    expect(response.token_type).toBe('bearer');
+    expect(response.access_token).toBeDefined();
+  });
+});
+
+test('should throw an exception with invalid keys', () => {
+  expect.assertions(1);
+
+  return getBearerToken('12345', 'abcde').catch(error => {
+    expect(error).toBeDefined();
   });
 });
