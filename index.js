57924b6917185a22a0f01e2eb1e02fc6587aebd8
Tien Do <tiendq@gmail.com>
2018-06-11 21:33:56 +0700

    refactor: redux-logger and redux-devtools-extension

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import logger from 'redux-logger';
 import * as reducers from './src/reducers';
 import TwitterApp from './src/twitter-app';
 
-const logger = createLogger();
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 // Configure redux-devtools: https://github.com/zalmoxisus/redux-devtools-extension
-const store = createStore(combineReducers(reducers), composeEnhancers(applyMiddleware(thunk, logger)));
+let store = createStore(combineReducers(reducers), composeWithDevTools(applyMiddleware(thunk, logger)));
 
 ReactDOM.render(<Provider store={store}><TwitterApp /></Provider>, document.getElementById('root'));
