fcb3372da2572f9460adfd6969d3ee568e9176ff
Tien Do <tiendq@gmail.com>
2018-06-18 17:53:54 +0700

    refactor: unnesting class selectors

--- src/client/components/twitter-app.js
+++ src/client/components/twitter-app.js
@@ -10,13 +10,13 @@ const TwitterApp = () => (
       <h1 className="col-12">twitter-client-redux</h1>
       <p className="col-12">You can search Twitter using the search box below</p>
     </header>
-    <div className="row">
+    <div className="row test-blue">
       <SearchFormContainer />
     </div>
     <div className="row">
       <StatusBanner />
     </div>
-    <div className="row">
+    <div className="row test-red">
       <SearchResultContainer />
     </div>
   </div>
